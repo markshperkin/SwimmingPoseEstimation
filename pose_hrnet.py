4d8d6d208f6ec10159c5386ddb5a90c3810565ac
mark shperkin <markshperkin1@gmail.com>
2024-12-06 16:59:56 -0500

    added requirements file

--- pose_hrnet.py
+++ pose_hrnet.py
@@ -5,9 +5,9 @@
 # Comented by Mark Shperkin
 # ------------------------------------------------------------------------------
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+# from __future__ import absolute_import
+# from __future__ import division
+# from __future__ import print_function
 
 import os
 import logging
